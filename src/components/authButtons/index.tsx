10f6056ea390f655d3fe5238a8c8aa4e20e5eda5
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-06 15:33:42 -0300

    adicionando a função signIn nos botões

--- src/components/authButtons/index.tsx
+++ src/components/authButtons/index.tsx
@@ -1,18 +1,37 @@
+import { signIn } from "next-auth/react"
 import { AuthButton, Container } from "./styles"
+import { useRouter } from "next/router"
+
+type AuthButtonsProps = {
+  callbackUrl?: string
+}
+
+export function AuthButtons({ callbackUrl = '/' }: AuthButtonsProps) {
+  const router = useRouter()
 
 
-export const AuthButtons = () => {
+  function handleSignIn(provider?: string) {
+    if (!provider) {
+      router.push(callbackUrl)
+      return 
+    }
+    
+    signIn(provider, {
+      callbackUrl
+    })
+  }
+
   return (
     <Container>
-      <AuthButton>
+      <AuthButton onClick={() => handleSignIn('google')}>
         <img src="/images/icons/google.svg" alt="Google Logo" />
         Entrar com Google
       </AuthButton>
-      <AuthButton>
+      <AuthButton onClick={() => handleSignIn('github')}>
         <img src="/images/icons/github.svg" alt="Github Logo" />
         Entrar com Github
       </AuthButton>
-      <AuthButton>
+      <AuthButton onClick={() => handleSignIn()}>
         <img src="/images/icons/rocket.svg" alt="Rocket Icon" />
         Entrar com visitante
       </AuthButton>
