b49dc135a2eeca8d4de8ba8feafad91ee1b64eac
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-06 14:20:40 -0300

    adicionando os arquivos globais de estilização

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Nunito } from 'next/font/google'
+import { globalStyles } from '@/styles/global'
+
+export const nunito = Nunito({
+  subsets: ['latin']
+})
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div
+      className={nunito.className}
+    >
+      <Component {...pageProps} />
+    </div>
+  )
 }
