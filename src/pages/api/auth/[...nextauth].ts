57a77f43619d47fba44de3a55b0f18b46ef6a4b3
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-06 15:32:48 -0300

    criando a autenticação com o Github

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,38 @@
+import { PrismaAdapter } from "@/lib/auth/prismaAdapter";
+import { NextApiRequest, NextApiResponse } from "next";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GithubProvider, { GithubProfile } from 'next-auth/providers/github'
+
+export function buildNextAuthOptions(req: NextApiRequest, res: NextApiResponse): NextAuthOptions {
+  return {
+    adapter: PrismaAdapter(req, res),
+    providers: [
+      GithubProvider({
+        clientId: process.env.GITHUB_CLIENT_ID ?? "",
+        clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
+        profile(profile: GithubProfile) {
+          return {
+            id: profile.id,
+            name: profile.name!,
+            email: profile.email!,
+            avatar_url: profile.avatar_url,
+          }
+        }
+      })
+    ],
+    callbacks: {
+      async session({ session, user }) {
+        return {
+          ...session,
+          user,
+        }
+      }
+    }
+  }
+}
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return NextAuth(req, res, buildNextAuthOptions(req, res))
+}
+
+
