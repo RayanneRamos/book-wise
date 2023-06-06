62c96aebcb0d18a66cef7bf87dff6f594c9294a9
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-06 15:33:54 -0300

    banco de dados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import { signOut, useSession } from "next-auth/react"
+
 export default function Home() {
+  const { data } = useSession()
+  
   return (
-    <h1>Hello World</h1>
+    <pre>
+      {JSON.stringify(data, null, 2)}
+      <button onClick={() => signOut()}>deslogar</button>
+    </pre>
   )
 }
