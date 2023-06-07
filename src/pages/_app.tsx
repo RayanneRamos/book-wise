e58c03c08f0f4d90b18bd806b5b60d9388f71cca
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-07 15:32:39 -0300

    fazendo a configuração de usar layouts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,16 @@ import type { AppProps } from 'next/app'
 import { Nunito } from 'next/font/google'
 import { globalStyles } from '@/styles/global'
 import { SessionProvider } from 'next-auth/react'
+import { ReactElement, ReactNode } from 'react'
+import { NextPage } from 'next'
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
 
 export const nunito = Nunito({
   subsets: ['latin']
@@ -9,13 +19,15 @@ export const nunito = Nunito({
 
 globalStyles()
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page)
+  
   return (
     <SessionProvider session={session}>
       <div
         className={nunito.className}
        >
-        <Component {...pageProps} />
+        {getLayout(<Component {...pageProps} />)}
       </div>
     </SessionProvider>
   )
