bc62abd6a7dcd5cc71e60b2d617bda756731c7b9
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-07 15:35:27 -0300

    criando o componente Sidebar e a sua estilização

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,40 @@
+import { signOut, useSession } from "next-auth/react"
+import { Navigation } from "../Navigation"
+import { Container, LoginButton, UserDetails } from "./styles"
+import { SignIn, SignOut } from "@phosphor-icons/react"
+import { Text } from "../Typography"
+import { Avatar } from "../ui/Avatar"
+import { useRouter } from "next/router"
+
+export const Sidebar = () => {
+  const { data: session } = useSession()
+  const router = useRouter()
+  const user = session?.user
+
+  function handleOpenProfile() {
+    router.push(`/profile/${user?.id}`)
+  }
+  
+  return (
+    <Container>
+      <div>
+        <img className="logo" src="/images/logo.svg" alt="BookWise Logo" />
+        <Navigation />
+      </div>
+      <footer>
+        { !user ? (
+          <LoginButton href="/login">
+            Fazer Login
+            <SignIn size={24} />
+          </LoginButton>
+        ) : (
+          <UserDetails>
+            <Avatar size='sm' src={user?.avatar_url} alt={user?.name} onClick={handleOpenProfile} css={{ cursor: 'pointer' }} />
+            <Text size='sm'>{user?.name}</Text>
+            <SignOut size={20} color="#f72a68" onClick={() => signOut()} />
+          </UserDetails>
+        ) }
+      </footer>
+    </Container>
+  )
+}
