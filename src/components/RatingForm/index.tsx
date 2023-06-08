a294fb5d8204c631bc964b55237c9bc677dbfc57
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-08 18:12:10 -0300

    criando o componente RatingForm e a sua estilização

---
+++ src/components/RatingForm/index.tsx
@@ -0,0 +1,65 @@
+import { useSession } from "next-auth/react"
+import { ActionsContainer, Container, FormContainer, UserDetails } from "./styles"
+import { Avatar } from "../ui/Avatar"
+import { Heading } from "../Typography"
+import { RatingStars } from "../RatingStars"
+import { FormEvent, useState } from "react"
+import { TextArea } from "../ui/Form/TextArea"
+import { ActionIcon } from "../ui/ActionIcon"
+import { Check, X } from "@phosphor-icons/react"
+import { useMutation } from "@tanstack/react-query"
+import { api } from "@/lib/axios"
+
+type RatingFormProps = {
+  onCancel: () => void
+  bookId: string
+}
+
+export const RatingForm = ({ onCancel, bookId }: RatingFormProps) => {
+  const { data: session } = useSession()
+  const user = session?.user
+  const [ currentRate, setcurrentRate ] = useState(0)
+  const [ description, setDescription ] = useState('')
+  const submitDisabled = !description.trim() || !currentRate
+  const { mutateAsync: handleRate } = useMutation(async () => {
+    await api.post(`/books/${bookId}/rate`, {
+      description,
+      rate: currentRate,
+    })
+  })
+  
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    if (submitDisabled) {
+      return
+    }
+
+    await handleRate()
+  }
+  
+  return (
+    <Container>
+      { user && (
+        <UserDetails>
+          <section>
+            <Avatar alt={user.name} src={user.avatar_url} />
+            <Heading size='xs'>{user.name}</Heading>
+          </section>
+          <RatingStars size='lg' rating={currentRate} setRating={setcurrentRate} />
+        </UserDetails>
+      ) }
+      <FormContainer onSubmit={handleSubmit}>
+        <TextArea 
+          placeholder="Escreva sua avaliação" 
+          maxLength={450} value={description} 
+          onChange={({ target }) => setDescription(target.value)} 
+        />
+        <ActionsContainer>
+          <ActionIcon type='button' onClick={onCancel} iconColor='purple100' icon={<X />} />
+          <ActionIcon type='submit' iconColor='green100' icon={<Check />} disabled={submitDisabled} />
+        </ActionsContainer>
+      </FormContainer>
+    </Container>
+  )
+}
