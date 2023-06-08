94e3b0e69c984378f15eb26a034c4f14c73145fe
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-08 14:44:00 -0300

    adicionando a url do unsplash para buscar as imagens

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com', 'github.com']
+    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com', 'github.com', 'images.unsplash.com']
   }
 }
 
