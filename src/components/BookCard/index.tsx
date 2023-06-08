17fcbefaf45a37656b3ca9b48d9b182909dd99f3
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-08 17:05:14 -0300

    adicionando o RatingDialog no componente

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -2,6 +2,7 @@ import { Book } from "@prisma/client"
 import { BookDetails, BookImage, BookName, Container, ReadBadge } from "./styles"
 import { Text } from "../Typography"
 import { RatingStars } from "../RatingStars"
+import { RatingsDialog } from "../RatingsDialog"
 
 export type BookWithAvgRating = Book & {
   avgRating: number
@@ -27,24 +28,26 @@ export const BookCard = ({ book, size = 'md' }: BookCardProps) => {
   const currentSize = IMAGES_SIZES[size]
   
   return (
-    <Container>
-      { book?.alreadyRead && (
-        <ReadBadge>LIDO</ReadBadge>
-      ) }
-      <BookImage 
-        width={currentSize.width} 
-        height={currentSize.height} 
-        alt={book.name} 
-        src={book.cover_url} 
-        css={{ minWidth: currentSize.width }} 
-      />
-      <BookDetails>
-        <div>
-          <BookName size='xs'>{book.name}</BookName>
-          <Text size='sm' color='gray-400'>{book.author}</Text>
-        </div>
-        <RatingStars rating={book.avgRating} />
-      </BookDetails>
-    </Container>
+    <RatingsDialog>
+      <Container>
+        { book?.alreadyRead && (
+          <ReadBadge>LIDO</ReadBadge>
+        ) }
+        <BookImage 
+          width={currentSize.width} 
+          height={currentSize.height} 
+          alt={book.name} 
+          src={book.cover_url} 
+          css={{ minWidth: currentSize.width }} 
+        />
+        <BookDetails>
+          <div>
+            <BookName size='xs'>{book.name}</BookName>
+            <Text size='sm' color='gray-400'>{book.author}</Text>
+          </div>
+          <RatingStars rating={book.avgRating} />
+        </BookDetails>
+      </Container>
+    </RatingsDialog>
   )
 }
