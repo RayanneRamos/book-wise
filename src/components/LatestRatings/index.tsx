458784b0fb077c25db0a5950e1e8ceaa3c2927af
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-08 14:44:32 -0300

    pegando de forma dinâmica os livros com as avaliações mais recentes

--- src/components/LatestRatings/index.tsx
+++ src/components/LatestRatings/index.tsx
@@ -2,9 +2,16 @@ import { ChartLineUp } from "@phosphor-icons/react"
 import { PageTitle } from "../ui/Pagetitle"
 import { Container } from "./styles"
 import { Text } from "../Typography"
-import { RatingCard } from "../RatingCard"
+import { RatingCard, RatingWithAuthorAndBook } from "../RatingCard"
+import { useQuery } from "@tanstack/react-query"
+import { api } from "@/lib/axios"
 
 export const LatestRatings = () => {
+  const { data: ratings } = useQuery<RatingWithAuthorAndBook[]>(['latest-ratings'], async () => {
+    const { data } = await api.get('/ratings/latest')
+    return data?.ratings ?? []
+  }) 
+  
   return (
     <Container>
       <PageTitle 
@@ -15,28 +22,9 @@ export const LatestRatings = () => {
       />
       <Text size='sm'>Avaliações mais recentes</Text>
       <section>
-        { Array.from({ length: 20 }).map((_, i) => {
+        { ratings?.map((rating) => {
           return (
-            <RatingCard key={i} rating={{
-              id: 'aa',
-              rate: 4,
-              user: {
-                name: 'John Doe',
-                avatar_url: 'https://github.com/RayanneRamos.png',
-                email: 'johndoe@gmail.com',
-                id: 'wefwefwef',
-                created_at: new Date(),
-              },
-              book: {
-                author: 'John Doe',
-                cover_url: 'https://github.com/RayanneRamos.png',
-                id: 'qijfiwf',
-                name: 'John Doe',
-                summary: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, delectus eaque omnis numquam earum tenetur eius voluptatem quaerat adipisci. Dolorem ea reiciendis consectetur nisi quia odio. Sequi earum sint fugiat!',
-                total_pages: 100,
-              },
-              created_at: new Date(),
-            }} />
+            <RatingCard key={rating.id} rating={rating} />
           )
         }) }
       </section>
