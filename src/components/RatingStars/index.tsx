fd3bdf8496d102cc58244f6655cc27592e687e3e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-08 18:16:11 -0300

    criando a função de permitir o usuário de avaliar um livro

--- src/components/RatingStars/index.tsx
+++ src/components/RatingStars/index.tsx
@@ -1,20 +1,47 @@
 import { ComponentProps } from "@stitches/react"
 import { Container } from "./styles"
 import { Star } from '@phosphor-icons/react'
+import { useState } from "react"
 
 type RatingStarsProps = ComponentProps<typeof Container> & {
   rating: number
   size?: 'sm' | 'md' | 'lg'
+  setRating?: (rating: number) => void
 }
 
-export const RatingStars = ({ rating, size = 'sm', ...props }: RatingStarsProps) => {
+export const RatingStars = ({ rating, size = 'sm', setRating, ...props }: RatingStarsProps) => {
+  const [ previewValue, setPreviewValue ] = useState(0)
+  const isEditable = !!setRating
+  const ratingValue = isEditable ? previewValue : rating
+
+  function handleMouseEnter(value: number) {
+    if (isEditable) {
+      setPreviewValue(value)
+    }
+  }
+
+  function handleMouseLeave() {
+    if (isEditable) {
+      setPreviewValue(rating)
+    }
+  }
+
+  function handleSetValue() {
+    if (isEditable) {
+      setRating(previewValue)
+    }
+  }
+  
   return (
-    <Container size={size} {...props}>
+    <Container css={isEditable ? { cursor: 'pointer'  } : undefined} size={size} {...props}>
       { Array.from({ length: 5 }).map((_, i) => {
         return (
           <Star  
             key={`star-${i}`}
-            weight={(i + 1) <= rating ? 'fill' : 'regular'}
+            weight={(i + 1) <= ratingValue ? 'fill' : 'regular'}
+            onMouseEnter={() => handleMouseEnter(i + 1)}
+            onMouseLeave={handleMouseLeave}
+            onClick={handleSetValue}
           />
         )
       })}
