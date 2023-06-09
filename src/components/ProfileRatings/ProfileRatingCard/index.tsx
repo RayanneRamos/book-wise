1788c58a0c165e0db37c618b5532ade74d483fba
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-09 18:16:03 -0300

    criando o componente ProfileRatings e a sua estilização

---
+++ src/components/ProfileRatings/ProfileRatingCard/index.tsx
@@ -0,0 +1,35 @@
+import { Heading, Text } from "@/components/Typography"
+import { ProfileRating } from ".."
+import { getRelativeTimeString } from "@/utils/getRelativeString"
+import { BookDetails, BookImage, CardContent, Container } from "./styles"
+import Link from "next/link"
+import { RatingStars } from "@/components/RatingStars"
+
+type ProfileRatingCardProps = {
+  rating: ProfileRating
+}
+
+export const ProfileRatingCard = ({ rating }: ProfileRatingCardProps) => {
+  const distance = getRelativeTimeString(new Date(rating.created_at), 'pt-br')
+  
+  return (
+    <Container>
+      <Text size='sm' color='gray-300'>{distance}</Text>
+      <CardContent>
+        <BookDetails>
+          <Link style={{ display: 'flex' }} href={`/explore?book=${rating.book_id}`}>
+            <BookImage src={rating.book.cover_url} alt={rating.book.name} width={98} height={134} />
+          </Link>
+          <section>
+            <div>
+              <Heading size='sm'>{rating.book.name}</Heading>
+              <Text size='sm' color='gray-400'>{rating.book.author}</Text>
+            </div>
+            <RatingStars rating={rating.rate} />
+          </section>
+        </BookDetails>
+        <Text size='sm' color='gray-300'>{rating.description}</Text>
+      </CardContent>
+    </Container>
+  )
+}
---
+++ src/components/ProfileRatings/index.tsx
@@ -0,0 +1,60 @@
+import { Book, CategoriesOnBooks, Category, Rating } from "@prisma/client"
+import { Container, RatingsList } from "./styles"
+import { PageTitle } from "../ui/Pagetitle"
+import { MagnifyingGlass, User } from "@phosphor-icons/react"
+import { Link } from "../ui/Link"
+import { Input } from "../ui/Form/input"
+import { useMemo, useState } from "react"
+import { ProfileRatingCard } from "./ProfileRatingCard"
+import { Text } from "../Typography"
+
+export type ProfileRating = Rating & {
+  book: Book & {
+    categories: CategoriesOnBooks & {
+      category: Category
+    }[]
+  }
+} 
+
+type ProfileRatingsProps = {
+  ratings: ProfileRating[]
+  isOwnProfile: boolean
+}
+
+export const ProfileRatings = ({ ratings, isOwnProfile }: ProfileRatingsProps) => {
+  const [ search, setSearch ] = useState('') 
+  const filteredRatings = useMemo(() => {
+    return ratings.filter(rating => {
+      return rating.book.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+    })
+  }, [ratings, search])
+  
+  return (
+    <Container>
+      { isOwnProfile ? (
+        <PageTitle title='Perfil' icon={<User size={25} />} />
+      ) : (
+        <Link href='/' text='voltar' iconSide='left' color='white' css={{ alignSelf: 'flex-start' }} />
+      ) }
+      <Input 
+        placeholder="Buscar livro avaliado"
+        icon={<MagnifyingGlass size={20} />}
+        css={{ marginTop: 40, marginBottom: 32 }}
+        value={search}
+        onChange={({ target }) => setSearch(target.value)}
+      />
+      <RatingsList>
+        {filteredRatings.map((rating) => {
+          return (
+            <ProfileRatingCard key={rating.id} rating={rating} />
+          )
+        })}
+        { filteredRatings.length <= 0 && (
+          <Text color='gray-400' css={{ textAlign: 'center' }}>
+            { search ? 'Nenhum livro encontrado' : 'Nenhuma avaliação encontrada'}
+          </Text>
+        )}
+      </RatingsList>
+    </Container>
+  )
+}
