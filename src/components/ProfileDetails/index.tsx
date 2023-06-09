da77fd682bee7e059811a95ab086d058c7a9ffdb
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-09 18:15:42 -0300

    criando o componente ProfileDetails e a sua estilização

---
+++ src/components/ProfileDetails/index.tsx
@@ -0,0 +1,48 @@
+import { ProfileData } from "@/pages/profile/[id]"
+import { Container, ProfileDetailsWrapper, UserInfo } from "./styles"
+import { Avatar } from "../ui/Avatar"
+import { Heading, Text } from "../Typography"
+import { ProfileDetailItem } from "./ProfileDetailItem"
+import { BookOpen, BookmarkSimple, Books, UserList } from "@phosphor-icons/react"
+
+type ProfileDetailsProps = {
+  profile: ProfileData
+}
+
+export const ProfileDetails = ({ profile }: ProfileDetailsProps) => {
+  const memberSinceYear = new Date(profile.user.member_since).getFullYear()
+  
+  return (
+    <Container>
+      <UserInfo>
+        <Avatar size='lg' alt={profile.user.name} src={profile.user.avatar_url} />
+        <Heading size='md' css={{ marginTop: 20 }}>{profile.user.name}</Heading>
+        <Text size='md' color='gray-400'>membro desde {memberSinceYear}</Text>
+      </UserInfo>
+      <ProfileDetailsWrapper>
+        <ProfileDetailItem 
+          icon={<BookOpen />}
+          info={profile.readPages}
+          label='Páginas lidas'
+        />
+        <ProfileDetailItem 
+          icon={<Books />}
+          info={profile.ratedBooks}
+          label='Livros avaliados'
+        />
+        <ProfileDetailItem 
+          icon={<UserList />}
+          info={profile.readAuthors}
+          label='Autores lidos'
+        />
+        { profile?.mostReadCategory && (
+          <ProfileDetailItem 
+            icon={<BookmarkSimple />}
+            info={profile.mostReadCategory}
+            label='Categoria mais lida'
+          />
+        ) }
+      </ProfileDetailsWrapper>
+    </Container>
+  )
+}
